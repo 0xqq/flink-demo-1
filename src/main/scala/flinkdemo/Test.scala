d663783d39d9f5183e3e33c94fb8de13fc2e1fe2
qufeipeng <qufeipeng@126.com>
2018-08-17 19:19:17 +0800

    add

--- src/main/scala/flinkdemo/Test.scala
+++ src/main/scala/flinkdemo/Test.scala
@@ -6,8 +6,9 @@ import java.util.Date
 object Test {
   def main(args: Array[String]): Unit = {
     val format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS")
-    val dt = (new Date).getTime + 20000;
+    val dt = (new Date).getTime + 10000;
     System.out.println(format.format(dt))
     System.out.println(dt + ",boos1,pc1,200.0")
+    System.out.println("000001," + dt)
   }
 }
